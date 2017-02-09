8792ba0c6515db64045571fd10ec9180dcd152f5
shawnkoon <shawn9405@gmail.com>
2017-02-08 23:49:50 -0800

    Added solution to P13 Large Sum. Used Linked Lists

--- Problem13/Problem13.java
+++ Problem13/Problem13.java
@@ -1,52 +1,89 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem13
-{
-   public static void main(String[] args)
-   {
-      ArrayList<Integer> digits = new ArrayList<Integer>();
+/*
+  Idea: I could be using biginteger datatype. But since it might run out of 
+        memory space, I will be using Linked Lists to do the operation.
+
+  Special: I will be populating Linked List in a reverse order of original number.
+           ex) 12345 -> [5, 4, 3, 2, 1]
+*/
+
+public class Problem13 {
+  
+  public static void main(String[] args) {
+    String filePath = "../textFiles/100numbers.txt";
+    
+    calculateSum(filePath);
+  }
+
+  private static void calculateSum(String filePath) {
+    List<Integer> sum = new LinkedList<Integer>();
+
+    try {
+      File fopen = new File(filePath);
+      Scanner fScan = new Scanner(fopen);
+
+      while( fScan.hasNext() ) {
+        List<Integer> cur = new LinkedList<Integer>();
+        String line = fScan.nextLine();
+        
+        for (char digit : line.toCharArray())
+          cur.add(0, Integer.parseInt(Character.toString(digit)));
+        
+        sum = addLists(sum, cur);
+      }// End of while.
+
+      fScan.close();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    String result = "";
+
+    for(int i = sum.size() - 1 ; i >= sum.size() - 10; i--) {
+      result += sum.get(i);
+    }
+    
+    System.out.println("The result is : " + result);
+  }// End of readFile();
+
+  private static List<Integer> addLists(List<Integer> list1, List<Integer> list2) {
+    List<Integer> total = new LinkedList<Integer>();
+
+    int maxLength = list1.size() > list2.size() ? list1.size() : list2.size();
+
+    for(int i = 0; i < maxLength; i++) {
+      int digit1 = 0, digit2 = 0, digit3 = 0;
+
+      if(i < list1.size())
+        digit1 = list1.get(i);
       
-      String fileName = "./textFiles/100numbers.txt";
-      Scanner kb;
-      File fname;
+      if(i < list2.size())
+        digit2 = list2.get(i);
       
-      try{
-         fname = new File(fileName);
-         
-         kb = new Scanner(fname);
-         
-         digits = solve(kb);
+      if(i < total.size())
+        digit3 = total.get(i);
+      
+      if(digit1 + digit2 + digit3 >= 10) {
+        if(i == total.size() - 1) {
+          total.set(i, (digit1 + digit2 + digit3) % 10);
+          total.add(i + 1, (digit1 + digit2 + digit3) / 10);
+        }
+        else {
+          total.add(i, (digit1 + digit2 + digit3) % 10);
+          total.add(i + 1, (digit1 + digit2 + digit3) / 10);
+        }
       }
-      catch(Exception e)
-      {
-         System.out.println("Please Check the fileName: "+fileName);
-         System.exit(-1);
+      else {
+        if(i == total.size() - 1)
+          total.set(i, (digit1 + digit2 + digit3));
+        else
+          total.add(i, (digit1 + digit2 + digit3));
       }
-      
-   }
-   
-   /* Idea: Take String in convert it into Integer arrayList (default start one into curNumber list)
-            scaner has next Line grab one more Line, insert it into the temp Integer arrayList
-            after that add and make it into new curNumber.
-            repeat untill every number has been added.
-   */
-   
-   public static ArrayList<Integer> solve(Scanner kb)
-   {
-      ArrayList<Integer> curNumber = new ArrayList<Integer>();
-      ArrayList<Integer> temp = new ArrayList<Integer>();
-      
-      while(kb.hasNext())
-      {
-         String s = kb.nextLine();
-         
-         for(int i = 0; i < s.length() ; i++)
-         {
-            
-         }
-      }
-      
-      return curNumber;
-   }
+    }
+
+    return total;
+  }// End of addLists().
 }
